b865fde9bafc27dc052d9b7db03cef706244bfc6
Jovan Nikolic <jovan.nikolic.dev@gmail.com>
2022-07-10 23:10:01 +0200

    enqueued style and script

--- functions.php
+++ functions.php
@@ -5,4 +5,13 @@
  * @package onsite
  */
 
-//function
+function onsite_enqueue_scripts(): void {
+	wp_register_style('onsite_stylesheet', get_stylesheet_uri());
+	wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', false, false, true);
+
+
+	wp_enqueue_style('onsite_stylesheet');
+	wp_enqueue_script('main-js');
+}
+
+add_action('wp_enqueue_scripts', 'onsite_enqueue_scripts');
